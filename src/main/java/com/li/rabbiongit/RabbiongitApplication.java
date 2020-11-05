2149a37fa08e123c162c558bfe3c2d752930ab16
lthlo <lthloveher@163.com>
2020-11-05 23:51:26 +0800

    提交代码

---
+++ src/main/java/com/li/rabbiongit/RabbiongitApplication.java
@@ -0,0 +1,14 @@
+package com.li.rabbiongit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbiongitApplication {
+
+    public static void main(String[] args) {
+        System.out.println(11111111);
+        SpringApplication.run(RabbiongitApplication.class, args);
+    }
+
+}
